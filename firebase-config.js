6731c517b70c18879adb9cb7bf4f28595a5cffdb
Noboyband <Noboyband@users.noreply.github.com>
2026-07-14 17:51:00 +0700

    Fix Firebase browser configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Firebase Web configuration. The application initializes Firebase in app.js.
+export const firebaseConfig = {
   apiKey: "AIzaSyDy_cTXagPC_G_Lioh32ufpioBqU2ddBd4",
   authDomain: "roddonjai-day-2026.firebaseapp.com",
   projectId: "roddonjai-day-2026",
@@ -12,6 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "501290589614",
   appId: "1:501290589614:web:5fa6d22c2f5883daf6bc3f"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
